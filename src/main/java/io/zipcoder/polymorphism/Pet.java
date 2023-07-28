046900c620349194d3511d9e0cba0c73bb2b35f8
nigel <bynum1001@gmail.com>
2023-07-28 11:53:36 -0400

    update

--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -1,25 +1,17 @@
 package io.zipcoder.polymorphism;
 
-public abstract class Pet { //abstract class is an object/blueprint but CANNOT create objects
+public abstract class Pet {
 
     private String name;
 
-    public Pet() {
+    public Pet() {}
 
-    }
+    public Pet(String name) {this.name = name;}
 
-    public Pet(String name) {
-        this.name = name;
-    }
+    public String getName() {return name;}
 
-    public String getName() {
-        return name;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public abstract String speak(); // in abstract classes, methods are defined with no code in the body
-
+    public abstract String speak();
 }
+//
